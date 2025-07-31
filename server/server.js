20ae1fe90b468b516b3eecf717218565aa6042a0
kiptalam1 <adamskiptalam0@gmail.com>
2025-07-31 15:06:24 +0300

    fix: fix path

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/client/dist")));
 
 	// handle any requests that don't match the above routes
-	app.get("/*", (req, res) => {
+	app.get("*", (req, res) => {
 		res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 	});
 }
