12f634418ef00a91af9929bd2a747549119980b6
kiptalam1 <adamskiptalam0@gmail.com>
2025-08-06 16:44:50 +0300

    fix: change app name.

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -7,10 +7,10 @@ const LandingPage = () => {
 			<div className="flex flex-col sm:flex-row items-center gap-12 w-full max-w-6xl">
 				<div className="flex-1 space-y-4 text-center sm:text-left">
 					<h1 className="font-[Poppins] text-5xl sm:text-6xl font-bold text-blue-600 leading-tight">
-						facebook
+						flocksy
 					</h1>
 					<p className="text-xl sm:text-2xl text-gray-700 dark:text-gray-200">
-						Facebook helps you connect and share{" "}
+						Flocksy helps you connect and share{" "}
 						<br className="hidden sm:block" />
 						with the people in your life.
 					</p>
